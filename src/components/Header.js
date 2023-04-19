201d254be247c38194d524b0a63a61dd18c2a872
ithmv <ithmv@vk.com>
2023-04-19 16:30:05 +0700

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Reg from './Reg';
 
 function Header() {
+  const [regDisplay, setRegDisplay] = useState('none');
 
-
+  const handleRegClick = () => {
+    setRegDisplay('flex');
+  };
 
   return (
     <div className="Header">
       <h1>Header</h1>
       <section>
-        <a id='reg' href='/'>Регистрация</a>
+        <a id='reg' href='#' onClick={handleRegClick}>Регистрация</a>
       </section>
-
+      <Reg display={regDisplay} setDisplay={setRegDisplay} />
     </div>
   );
 }
--- src/components/Reg.js
+++ src/components/Reg.js
@@ -1,12 +1,41 @@
-import '../styles/style.css';
-function Reg() {
+import React, { useState } from 'react';
+
+function Reg({ display, setDisplay }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleClose = () => {
+    setDisplay('none');
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ name, email });
+  };
+
   return (
-    <div className="Reg">
-      <input type='text' placeholder='Имя' />
-      <input type='email' placeholder='Электронная почта' />
-      <input type='password' placeholder='Пароль' />
-      <button>Зарегистрироваться</button>
-      <button>Закрыть</button>
+    <div className="Reg" style={{ display }}>
+      <h2>Registration Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Name:
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Email:
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </label>
+        </div>
+        <div>
+          <button type="submit">Зарегистрироваться</button>
+        </div>
+      </form>
+      <div>
+        <button onClick={handleClose}>Close</button>
+      </div>
     </div>
   );
 }
