3665332e15cb241ea9364e449dc6f93c2740ad20
ithmv <ithmv@vk.com>
2023-04-19 14:19:45 +0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import '../styles/style.css';
+
+function Header() {
+  return (
+    <div className="Header">
+      <h1>Mgs.me</h1>
+      <section>
+        <a href=''>Log In</a>
+        <a href=''>Sign Up</a>
+      </section>
+    </div>
+  );
+}
+
+export default Header;
